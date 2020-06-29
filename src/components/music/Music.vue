dca96cf9edb9d984af24fb65c1336322e6a28bbb
cyf <cyf-ms@hotmail.com>
2020-06-29 11:16:16 +0800

    全员 https 网站为中文

--- src/components/music/Music.vue
+++ src/components/music/Music.vue
@@ -2,7 +2,7 @@
 <div class="container">
     <b-alert show variant="warning">由于浏览器跨域访问的限制，本页面只提供歌曲预览而无法播放。<br>如需要完整的服务请点击<a href="http://se.cyf-cloud.cn:5555" class="alert-link" target="_blank" security="restricted" sandbox="">这里</a>
     </b-alert>
-    <iframe class="cc-iframe" src="http://se.cyf-cloud.cn:5555" security="restricted" sandbox="allow-scripts allow-same-origin allow-popups"></iframe>
+    <iframe class="cc-iframe" src="https://se.cyf-cloud.cn:5555" security="restricted" sandbox="allow-scripts allow-same-origin allow-popups"></iframe>
     <b-badge pill variant="info" href="http://www.subsonic.org/">Powered by Subsonic©</b-badge>
     <b-badge pill variant="warning" href="https://jq.qq.com/?_wv=1027&k=isIw44iL">加入QQ群以获取账户密码</b-badge>
     <br>
--- src/components/music/Upload.vue
+++ src/components/music/Upload.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
     <b-alert show >为防止滥传占用带宽，我们进行了群内分享上传账户的机制。<br>如果需要请<a href="https://jq.qq.com/?_wv=1027&k=isIw44iL" class="alert-link">加入群</a>。</b-alert>
-    <iframe class="cc-iframe" src="http://se.cyf-cloud.cn:5225" ></iframe>
+    <iframe class="cc-iframe" src="https://se.cyf-cloud.cn:5225" ></iframe>
     <b-badge pill variant="info" href="https://www.rejetto.com/hfs/">Powered by HFS</b-badge>
 </div>
 </template>
