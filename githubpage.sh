f3b0e7097c8da44cfa97f6694e21e707cd801374
cyf-gh <cyf-ms@hotmail.com>
2020-11-08 13:57:18 +0800

    部署到githubpage

---
+++ githubpage.sh
@@ -0,0 +1,14 @@
+###
+ # @Date: 2020-11-08 13:50:16
+ # @LastEditors: cyf
+ # @LastEditTime: 2020-11-08 13:56:11
+ # @FilePath: \cyf-cloud.front\githubpage.sh
+ # @Description: What is mind? No matter. What is matter? Nevermind.
+### 
+cd /home/cyf/.web/cyf-cloud.front
+git clone http://github.com/cyf-gh/cyf-gh.github.io
+git cp -R ./mycli3/* ./cyf-gh.github.io
+cd ./cyf-gh.github.io
+git add .
+git commit -m "Auto deploy to github.io"
+git push
