0205ef6df369e549af79accd7c65a7a7c1e4ed78
cyf-gh <cyf-ms@hotmail.com>
2020-11-08 14:09:48 +0800

    修复错误

--- githubpage.sh
+++ githubpage.sh
@@ -1,12 +1,13 @@
 ###
  # @Date: 2020-11-08 13:50:16
  # @LastEditors: cyf
- # @LastEditTime: 2020-11-08 14:04:31
+ # @LastEditTime: 2020-11-08 14:09:17
  # @FilePath: \cyf-cloud.front\githubpage.sh
  # @Description: What is mind? No matter. What is matter? Nevermind.
 ### 
 cd /home/cyf/.web/cyf-cloud.front
 git clone --depth 1 http://github.com/cyf-gh/cyf-gh.github.io
+sudo npm run build_gp
 git cp -R ./mycli3/* ./cyf-gh.github.io
 
 cd ./cyf-gh.github.io
