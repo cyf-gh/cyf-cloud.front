85115460eb1cd5964bdc790b39ffc0391c6a07bf
cyf <cyf-ms@hotmail.com>
2020-11-09 20:40:19 +0800

    v1x1-fix-001a1
    解决阅读页面移动端较窄的布局问题

--- src/components/post/Reader.vue
+++ src/components/post/Reader.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2020-10-07 19:34:34
  * @LastEditors: cyf
- * @LastEditTime: 2020-11-01 14:06:04
+ * @LastEditTime: 2020-11-09 20:38:59
  * @FilePath: \cyf-cloud.front\src\components\post\Reader.vue
  * @Description: What is mind? No matter. What is matter? Nevermind.
 -->
 <template>
-<b-container>
-    <b-card v-if="!isPrivate">
-        <b-container style="background-color:white" v-if="post != null">
+<div class="mt-4 mx-2">
+    <div v-if="!isPrivate">
+        <b-container style="background-color:white" v-if="post != null" fluid>
             <br>
             <b-row>
             <b-col><h2>{{post.Title}}</h2></b-col>
@@ -49,14 +49,14 @@
                 </b-nav>
             </b-navbar>
         </b-container>
-    </b-card>
-    <b-card v-else class="text-center">
+    </div>
+    <div v-else class="text-center">
         <h2>你访问的文章不公开</h2>
         <br>
         <b-avatar size="4em" variant="warning">闭门<br>羹</b-avatar>
         <h3 class="mt-4"><b-badge href="/posts" variant="light">看看其他</b-badge> </h3>
-    </b-card>
-</b-container>
+    </div>
+</div>
 </template>
 
 <script>
